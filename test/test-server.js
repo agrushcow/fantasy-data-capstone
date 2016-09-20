46c1d21a308a3cdf6f870774a68dd4994ac96001
Adam Grushcow <adamgrushcow@gmail.com>
2016-09-20 03:47:44 +0000

    Implemented mocha test and set up node and file structure

---
+++ test/test-server.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+//var server = require('../server.js');
+//var Item = require('../models/item');
+
+var should = chai.should();
+//var app = server.app;
+
+ chai.use(chaiHttp);
